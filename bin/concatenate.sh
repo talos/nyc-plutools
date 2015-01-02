c388d371d651f84d65add3f23eda77b1360a77ea
John Krauss <irving.krauss@gmail.com>
2015-01-01 21:26:57 -0500

    improvements to concatenate

--- bin/concatenate.sh
+++ bin/concatenate.sh
@@ -1,3 +1,5 @@
+#!/bin/bash -e
+
 source bin/utils.sh
 
 info 'Concatenating borough PLUTO data in `./data/unzipped`'
@@ -12,21 +14,20 @@ for release in data/unzipped/*/; do
       ls $release*.csv $release*.CSV $release*.txt $release*.TXT 2>/dev/null | \
       head -n 1 | \
       xargs head -n 1 | \
-      tr -d '\r' | \
-      sed -E 's/ +/ /g' | \
-      sed -E 's/," "/,/g' \
+      tr -d '\r\0' | \
+      sed -E 's/ +/ /g' \
       > data/concatenated/$base.csv
 
     # Pipe in data
     LANG="CP-1252" \
       ls $release*.csv $release*.CSV $release*.txt $release*.TXT 2>/dev/null | \
       xargs tail -q -n +2 | \
-      tr -d '\r' | \
-      sed -E 's/ +/ /g' | \
-      sed -E 's/," "/,/g' \
+      tr -d '\r\0' | \
+      sed 's/[^[:print:]]//' | \
+      sed -E 's/ +/ /g' \
       >> data/concatenated/$base.csv
 
-    rm $release*.csv $release*.CSV $release*.txt $release*.TXT
+    #rm $release*.csv $release*.CSV $release*.txt $release*.TXT
   else
     info "Release $base already exists at data/concatenated/$base.csv"
   fi
